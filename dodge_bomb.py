3d1601e7fc40ab0d720f25dd4ff06b1ce2ff99eb
c0a25078 <c0a2507863@edu.teu.ac.jp>
2026-04-21 14:22:48 +0900

    爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame as pg
 
 
@@ -20,6 +21,15 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))  #爆弾用の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #爆弾を描く
+    bb_img.set_colorkey((0, 0, 0)) #黒い枠を透明にする
+    bb_rct = bb_img.get_rect()  #爆弾Rectを取得する
+    bb_rct.centerx = random.randint(0, WIDTH)   #爆弾の初期座標x
+    bb_rct.centery = random.randint(0, HEIGHT)  #爆弾の初期座標y
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,6 +54,8 @@ def main():
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)  #爆弾を表示させる
         pg.display.update()
         tmr += 1
         clock.tick(50)
